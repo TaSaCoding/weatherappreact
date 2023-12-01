54b05eb48f8f3b046dc1bf9862784270c83fa95b
TaSaCoding <142542869+TaSaCoding@users.noreply.github.com>
2023-12-01 17:53:59 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://tsd-portfolio.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Tanja Sauer
-        </a>
-        {""} and is {""}
-        <a
-          href="https://github.com/TaSaCoding/weatherappreact.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-soured
-        </a>{" "}
-        on GitHub
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://tsd-portfolio.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Tanja Sauer
+          </a>
+          {""} and is {""}
+          <a
+            href="https://github.com/TaSaCoding/weatherappreact.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-soured
+          </a>{" "}
+          on GitHub
+        </footer>
+      </div>
     </div>
   );
 }
